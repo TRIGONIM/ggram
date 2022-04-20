e5b7f33a0903b7127f3dcb6559a5255199f81e91
_AMD_ <amd@default.im>
2022-04-20 05:53:11 +0300

    add some info

--- examples/echo.lua
+++ examples/echo.lua
@@ -1,5 +1,12 @@
 -- #TODO write details about ctx object
 
+-- Run this file with
+-- lua echo.lua tmp
+-- where tmp is a directory to store temporary files like polling offset
+
+require("ggram.core")
+
+
 local bot = ggram("123456789:QWERTYUIOPASDFGHJKLZXCVBNM") -- replace with your token (t.me/BotFather)
 bot.enable_polling() -- start getUpdates loop
 
