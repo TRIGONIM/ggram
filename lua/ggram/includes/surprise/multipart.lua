0c9fed26e1b8b42e42615d54ce5574c366e15efb
_AMD_ <amd@default.im>
2022-05-08 11:39:10 +0300

    add reply.mediaGroupFromFiles for multipart data

--- lua/ggram/includes/surprise/multipart.lua
+++ lua/ggram/includes/surprise/multipart.lua
@@ -122,3 +122,22 @@ function RESP:photoFromFile(file_content, name)
 	local form_data = multipart.FormData():AddFile("photo", file_content, name)
 	return self.sendMultipart("sendPhoto", form_data)
 end
+
+function RESP:mediaGroupFromFiles(files) -- {{type = "photo", media = RAW_DATA, caption...}, ...}
+	local form_data = multipart.FormData()
+
+	local tbl = {}
+	for i,f in ipairs(files) do
+		form_data:AddFile("unique" .. i, f.media, "anydata?")
+		tbl[i] = {
+			type  = f.type,
+			media = "attach://unique" .. i,
+		}
+		for k,v in pairs(f) do -- merge additional File properties if not exists
+			tbl[i][k] = tbl[i][k] or v
+		end
+	end
+
+	form_data:AddTable("media", tbl)
+	return self.sendMultipart("sendMediaGroup", form_data)
+end
