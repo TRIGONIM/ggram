fb146a172e2f81879fdf188a97d5c63ff402b220
_AMD_ <amd@default.im>
2023-07-04 03:31:19 +0300

    🔥 bot.enable_polling() теперь работает на корутинах

--- lua/ggram/extensions/polling.lua
+++ lua/ggram/extensions/polling.lua
@@ -1,7 +1,14 @@
 local BOT_MT = debug.getregistry().GG_BOT
 
-local function getUpdates(bot, parameters)
-	return bot.call_method("getUpdates", parameters)
+local coroutinize  = ggram.include("utils.coro").coroutinize
+local def_to_yield = ggram.include("utils.coro").deferred_to_yield
+
+local function co_sleep(seconds)
+	coroutine.yield(function(cont) timer.Simple(seconds, cont) end)
+end
+
+local function co_call_method(bot, method, parameters)
+	return def_to_yield( bot.call_method )(method, parameters)
 end
 
 local function log(msg, ...)
@@ -9,7 +16,7 @@ local function log(msg, ...)
 	print(msg:format(...))
 end
 
-local function processUpdates(bot, updates)
+local process_updates = function(bot, updates)
 	if #updates > 0 then
 		local lastUpdate = updates[#updates]
 		log("#updates: %d, Last ID: %s", #updates, lastUpdate.update_id)
@@ -24,56 +31,57 @@ local function processUpdates(bot, updates)
 	end
 end
 
-local function poll(bot)
-	local key = "gg:polling_offset:" .. bot.id
-	local offset = cookie.GetNumber(key)
-	log("poll offset %s", offset or "none")
-
-	return getUpdates(bot, {timeout = 30, offset = offset})
-		:next(function(updates)
-			log("Received %d updates", #updates)
-			processUpdates(bot, updates)
-		end)
+local co_poll = function(bot)
+	local offset = cookie.GetNumber("gg:polling_offset:" .. bot.id)
+	log("co_poll(bot) offset %s", offset or "none")
+	return co_call_method(bot, "getUpdates", {timeout = 30, offset = offset})
 end
 
-local function handlePollError(self, err)
-	log("err after poll(self): %s", err)
+local co_on_error = function(bot, err)
+	log("err after co_poll(bot): %s", err)
 
 	if err.error_code == 409 then
 		if err.description:match("terminated by other getUpdates") then
 			log("terminated")
-			error("terminated")
+			print("ggram: Polling stopped: " .. err.description)
+			bot.polling = false
 		else
 			log("Deleting webhook")
-			return self.call_method("deleteWebhook", {})
+			local ok = co_call_method(bot, "deleteWebhook", {})
+			print(ok and "Webhook deleted" or "Webhook deletion error")
 		end
 	else
 		local wait = err.parameters and err.parameters.retry_after or 5
 		log("err. Waiting %d sec", wait)
-		return deferred.sleep(wait)
+		co_sleep(wait)
+	end
+end
+
+local polling_loop = function(bot)
+	while bot.polling do
+		local ok, res_err = co_poll(bot)
+		if not ok then
+			co_on_error(bot, res_err)
+		else
+			log("Received %d updates", #res_err)
+			process_updates(bot, res_err)
+		end
 	end
 end
 
 function BOT_MT:enable_polling()
+	log("call BOT_MT:enable_polling()")
 	self.polling = true
-	log("enable_polling")
 
-	local function restart_polling()
-		if not self.polling then log("polling disabled") return end
-		log("restart polling")
-		self:enable_polling()
-	end
-
-	deferred.sleep(0)
-		:next(function() return poll(self) end)
-		:next(nil, function(err) return handlePollError(self, err) end)
-		:next(restart_polling, error)
+	coroutinize(function()
+		co_sleep(0) -- for garry's mod (http not working at the first tick)
+		polling_loop(self)
+	end)
 end
 
 -- GG_POLL_LOG = true
--- local bot = ggram( require("env").test_bot )
--- bot.call_method("deleteWebhook", {}):next(PRINT, PRINT)
--- -- poll(bot)
+-- local bot = ggram( require("env").tg_test )
+-- -- bot.call_method("deleteWebhook", {}):next(PRINT, PRINT)
 -- bot.enable_polling()
 
 -- bot.text(function(ctx) ctx.reply.text(ctx.text) end, "asd")
